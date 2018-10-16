392009319d443e4497ada25a92fb89b9a42c214d
Maciej-R <macrysnik@gmail.com>
2018-10-16 14:09:36 +0200

    Maciek

--- Lab2.cc
+++ Lab2.cc
@@ -2,12 +2,18 @@
 #include <iostream>
 using namespace std;
 
-//To jest komentaż do tego pliku. Ten plik jest ewidentnie źle fsormatowany i należy to koniecznie poprawić, pamiętając o nazwach zmiennych
-//dopuszczalnej długości linii, znakach z kodu ANSI, wcięciach i dbalości o wsykoiehj jakosci kometarze
+//To jest komentaż do tego pliku.
+//Ten plik jest ewidentnie zle fsormatowany
+//i należy to koniecznie poprawic, pamietajac o nazwach zmiennych
+//dopuszczalnej długosci linii, znakach z kodu ANSI,
+//wcieciach i dbalosci o wysokiej jakosci kometarze
 
-int main() { int PierwszaZmienna=5; int Drugazmienna=7;
-int WynikDziałania=PierwszaZmienna+Drugazmienna;
+int main() {
+  int pierwsza=5;
+  int druga=7;
+  int wynikdzialnia=pierwsza+druga;
 
-cout <<"Wynik Dodawania Dwóch Liczb To:"<<WynikDziałania<<endl; // Wypisuje wynik dzialania
-return 0;
+  // Wypisuje wynik dzialania
+  cout <<"Wynik Dodawania Dwóch Liczb To:"<<wynikdzialania<<endl;
+  return 0;
 }
